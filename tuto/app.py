f582c6b44e43d0b433b96601fb68afee7e3ebbb2
Niveau Samuel <o22204836@or-iut-i002-f13.iut45.univ-orleans.fr>
2024-09-11 10:40:49 +0200

    intégration de la bdd

--- tuto/app.py
+++ tuto/app.py
@@ -1,5 +1,14 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap5
 app = Flask(__name__)
+import os.path
 app.config['BOOTSTRAP_SERVE_LOCAL'] = True
 bootstrap = Bootstrap5(app)
+def mkpath(p):
+    return os.path.normpath(os.path.join(
+os.path.dirname(__file__),
+p))
+from flask_sqlalchemy import SQLAlchemy
+app.config['SQLALCHEMY_DATABASE_URI'] = (
+'sqlite:///'+mkpath('../myapp.db'))
+db = SQLAlchemy(app)
